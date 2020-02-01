cd45cecbd2ec8b840ae33220c74adbe53e0499a4
unknown <samiulgg12@gmail.com>
2020-02-01 13:26:45 +0600

    Updates

---
+++ precache-manifest.12b9f0ce1d8856a3cd85959377c2d3c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296ad9eaa4ecda413fc485371f3a1b3c",
+    "url": "/adorableavatar/index.html"
+  },
+  {
+    "revision": "a2d84e8a9c28f989297d",
+    "url": "/adorableavatar/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "12f0a206040746643836",
+    "url": "/adorableavatar/static/css/main.aca72c8e.chunk.css"
+  },
+  {
+    "revision": "a2d84e8a9c28f989297d",
+    "url": "/adorableavatar/static/js/2.66cf0927.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adorableavatar/static/js/2.66cf0927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12f0a206040746643836",
+    "url": "/adorableavatar/static/js/main.a64be2f4.chunk.js"
+  },
+  {
+    "revision": "6c68c0ae459a516f20ca",
+    "url": "/adorableavatar/static/js/runtime-main.589cc66d.js"
+  },
+  {
+    "revision": "4318499e57ec0c08d42ffaf696788165",
+    "url": "/adorableavatar/static/media/BebasNeue Regular.4318499e.woff"
+  }
+]);
